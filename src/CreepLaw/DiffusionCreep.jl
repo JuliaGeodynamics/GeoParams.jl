00608dd028fdb01173eb0f0369522339cd3ad204
Nils Medinger <64605454+Dr3ven@users.noreply.github.com>
2021-12-07 11:55:04 +0100

    changed grain size exponent from m to p. Added meter dimension in A and changed CreepLawVariables from p to b

--- src/CreepLaw/DiffusionCreep.jl
+++ src/CreepLaw/DiffusionCreep.jl
@@ -18,8 +18,8 @@ Apparatus defines the appartus type that shall be recreated (Axial Compression, 
     equation::LaTeXString = "to be updated"
     n::GeoUnit            = 1.0NoUnits         # power-law exponent
     r::GeoUnit            = 0.0NoUnits         # exponent of water-fugacity
-    m::GeoUnit            = 3.0NoUnits         # grain size exponent
-    A::GeoUnit            = 1.5MPa^(-n-r)/s    # material specific rheological parameter
+    p::GeoUnit            = -3.0NoUnits         # grain size exponent
+    A::GeoUnit            = 1.5MPa^(-n-r)*s^(-1)*(1e-6m)^(-p)    # material specific rheological parameter
     E::GeoUnit            = 500kJ/mol          # activation energy
     V::GeoUnit            = 6e-6m^3/mol        # activation volume
     R::GeoUnit            = 8.314J/mol/K       # universal gas constant
@@ -30,19 +30,19 @@ end
 
 
 
-function ComputeCreepLaw_EpsII(TauII, a::DiffusionCreep, p::CreepLawVariables)
+function ComputeCreepLaw_EpsII(TauII, a::DiffusionCreep, b::CreepLawVariables)
     @unpack n         = a
     @unpack r         = a
-    @unpack m         = a
+    @unpack p         = a
     @unpack A         = a
     @unpack E         = a
     @unpack V         = a
     @unpack R         = a
     @unpack Apparatus = a
-    @unpack P         = p
-    @unpack T         = p
-    @unpack d         = p
-    @unpack f         = p
+    @unpack P         = b
+    @unpack T         = b
+    @unpack d         = b
+    @unpack f         = b
     if Apparatus == "AxialCompression"
         FT = sqrt(3.0)NoUnits
         FE = 2.0/sqrt(3.0)NoUnits
@@ -53,22 +53,22 @@ function ComputeCreepLaw_EpsII(TauII, a::DiffusionCreep, p::CreepLawVariables)
         FT = 1.0NoUnits
         FE = 1.0NoUnits
     end
-    return A.val*(TauII.val*FT)^n.val*d.val^-m.val*f.val^r.val*exp(-(E.val+P.val*V.val)/(R.val*T.val))/FE
+    return A.val*(TauII.val*FT)^n.val*d.val^-p.val*f.val^r.val*exp(-(E.val+P.val*V.val)/(R.val*T.val))/FE
 end
 
-function ComputeCreepLaw_TauII(EpsII, a::DiffusionCreep, p::CreepLawVariables)
+function ComputeCreepLaw_TauII(EpsII, a::DiffusionCreep, b::CreepLawVariables)
     @unpack n = a
     @unpack r = a
-    @unpack m = a
+    @unpack p = a
     @unpack A = a
     @unpack E = a
     @unpack V = a
     @unpack R = a
     @unpack Apparatus = a
-    @unpack P = p
-    @unpack T = p
-    @unpack d = p
-    @unpack f = p
+    @unpack P = b
+    @unpack T = b
+    @unpack d = b
+    @unpack f = b
     if Apparatus == "AxialCompression"
         FT = sqrt(3.0)NoUnits               # relation between differential stress recorded by apparatus and TauII
         FE = 2.0/sqrt(3.0)NoUnits           # relation between gamma recorded by apparatus and EpsII
@@ -79,13 +79,13 @@ function ComputeCreepLaw_TauII(EpsII, a::DiffusionCreep, p::CreepLawVariables)
         FT = 1.0NoUnits
         FE = 1.0NoUnits
     end
-    return A.val^(-1/n.val)*(EpsII.val*FE)^(1/n.val)*d.val^(-m.val/n.val)*f.val^(-r.val/n.val)*exp((E.val+P.val*V.val)/(n.val*R.val*T.val))/FT;
+    return A.val^(-1/n.val)*(EpsII.val*FE)^(1/n.val)*d.val^(-p.val/n.val)*f.val^(-r.val/n.val)*exp((E.val+P.val*V.val)/(n.val*R.val*T.val))/FT;
 end
 
 
 # Print info 
 function show(io::IO, g::DiffusionCreep)  
-    print(io, "DiffusionCreep: n=$(g.n.val), r=$(g.r.val), m=$(g.m.val), A=$(g.A.val), E=$(g.E.val), V=$(g.V.val), Apparatus=$(g.Apparatus)" )  
+    print(io, "DiffusionCreep: n=$(g.n.val), r=$(g.r.val), p=$(g.p.val), A=$(g.A.val), E=$(g.E.val), V=$(g.V.val), Apparatus=$(g.Apparatus)" )  
 end
 
 # predefined diffusion creep laws are to be added in the dictionary as it is done for dislocation creep laws (see 'DislocationCreep.jl')!
