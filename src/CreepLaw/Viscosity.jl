e6ea8c8674159086f206f930b11bb9e9ebab310f
albert-de-montserrat <58044444+albert-de-montserrat@users.noreply.github.com>
2022-05-19 16:58:26 +0200

    Update Viscosity.jl

--- src/CreepLaw/Viscosity.jl
+++ src/CreepLaw/Viscosity.jl
@@ -33,7 +33,7 @@ function local_iterations_EpsII(εII, v::Tuple, args; tol = 1e-6)
     while ϵ > tol  # Newton
         iter += 1
         f = εII - strain_rate_circuit(τII, v, args)
-        dfdτII =  - dεII_dτII(rand(), v, args)
+        dfdτII =  - dεII_dτII(τII, v, args)
         τII = τII - f / dfdτII
 
         ϵ = abs(τII-τII_prev)/τII
