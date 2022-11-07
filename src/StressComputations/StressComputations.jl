14e59848a4ddf9cc29eb2b5b08bf6a3830aa33a5
albert-de-montserrat <albert.de.montserrat@gmail.com>
2022-11-07 17:12:49 +0100

    stress for staggered grids with 1 phase

--- src/StressComputations/StressComputations.jl
+++ src/StressComputations/StressComputations.jl
@@ -22,6 +22,29 @@ function compute_τij(v, εij::NTuple{N,T}, args, τij_old::NTuple{N,T}) where {
     return τij, τII
 end
 
+function compute_τij(v, εij::NTuple{N,Any}, args, τij_old::NTuple{N,Any}) where {N}
+
+    # Second invariant of effective strainrate (taking elasticity into account)
+    #ε_eff = εij .+ 0.5.*τij_old./(1.0*args.dt)
+    ε_eff = effective_ε(εij, v, τij_old, args.dt)
+    εII   = second_invariant(ε_eff...)
+    ε_eff_averaged = naverage(ε_eff)
+
+    args  = merge(args, (τII_old=0,))
+    τII   = first(compute_τII(v, εII, args))
+    η_eff = 0.5*τII/εII
+    τij   = 2*η_eff.*ε_eff_averaged
+
+    return τij, τII
+end
+
+function naverage(x::NTuple{N,Any}) where N
+    ntuple( i -> naverage(x[i]), Val(N))
+end
+
+naverage(x::NTuple{N,T}) where {N,T} = sum(xi for xi in x)/N
+naverage(x::T) where {T<:Number} = x
+
 function compute_τij(v::NTuple{N1, AbstractMaterialParamsStruct}, εij::NTuple{N2,T}, args, τij_old::NTuple{N2,T}, phase::I) where {T,N1,N2,I<:Integer}
 
     # Second invariant of effective strainrate (taking elasticity into account)
