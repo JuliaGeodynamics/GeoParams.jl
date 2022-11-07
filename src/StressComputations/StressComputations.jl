c6c5017886e8f5211c826f008fbe362c02e27427
albert-de-montserrat <albert.de.montserrat@gmail.com>
2022-11-07 17:27:55 +0100

    apply some polish

--- src/StressComputations/StressComputations.jl
+++ src/StressComputations/StressComputations.jl
@@ -25,10 +25,9 @@ end
 function compute_τij(v, εij::NTuple{N,Any}, args, τij_old::NTuple{N,Any}) where {N}
 
     # Second invariant of effective strainrate (taking elasticity into account)
-    #ε_eff = εij .+ 0.5.*τij_old./(1.0*args.dt)
     ε_eff = effective_ε(εij, v, τij_old, args.dt)
     εII   = second_invariant(ε_eff...)
-    ε_eff_averaged = naverage(ε_eff)
+    ε_eff_averaged = staggered_tensor_average(ε_eff)
 
     args  = merge(args, (τII_old=0,))
     τII   = first(compute_τII(v, εII, args))
@@ -38,17 +37,9 @@ function compute_τij(v, εij::NTuple{N,Any}, args, τij_old::NTuple{N,Any}) whe
     return τij, τII
 end
 
-function naverage(x::NTuple{N,Any}) where N
-    ntuple( i -> naverage(x[i]), Val(N))
-end
-
-naverage(x::NTuple{N,T}) where {N,T} = sum(xi for xi in x)/N
-naverage(x::T) where {T<:Number} = x
-
 function compute_τij(v::NTuple{N1, AbstractMaterialParamsStruct}, εij::NTuple{N2,T}, args, τij_old::NTuple{N2,T}, phase::I) where {T,N1,N2,I<:Integer}
 
     # Second invariant of effective strainrate (taking elasticity into account)
-    #ε_eff = εij .+ 0.5.*τij_old./(1.0*args.dt)
     ε_eff = effective_ε(εij, v, τij_old, args.dt, phase)
     εII   = second_invariant(ε_eff)
     
@@ -59,3 +50,10 @@ function compute_τij(v::NTuple{N1, AbstractMaterialParamsStruct}, εij::NTuple{
 
     return τij, τII, η_eff
 end
+
+function staggered_tensor_average(x::NTuple{N,Union{T,NTuple{4,T}}}) where {N,T}
+    ntuple(i -> staggered_tensor_average(x[i]), Val(N))
+end
+
+staggered_tensor_average(x::NTuple{N,T}) where {N,T} = sum(xi for xi in x)/N
+staggered_tensor_average(x::T) where {T<:Number} = x
