926a78542d0f3566b788c17859319e309421daf9
albert-de-montserrat <albert.de.montserrat@gmail.com>
2022-11-08 17:17:27 +0100

    in-place methods to compute stress

--- src/StressComputations/StressComputations.jl
+++ src/StressComputations/StressComputations.jl
@@ -24,7 +24,9 @@ function compute_τij(v, εij::NTuple{N,T}, args, τij_old::NTuple{N,T}) where {
 end
 
 # Single material phase, staggered grid
-function compute_τij(v, εij::NTuple{N,Union{T,NTuple{4,T}}}, args, τij_old::NTuple{N,Union{T,NTuple{4,T}}}) where {N,T}
+function compute_τij(
+    v, εij::NTuple{N,Union{T,NTuple{4,T}}}, args, τij_old::NTuple{N,Union{T,NTuple{4,T}}}
+) where {N,T}
 
     # Second invariant of effective strainrate (taking elasticity into account)
     ε_eff = effective_ε(εij, v, τij_old, args.dt)
@@ -80,6 +82,99 @@ function compute_τij(
 
     return τij, τII, η_eff
 end
+
+# full stress calculation routines
+
+# collocated grid
+function compute_τij!(
+    Txx, Tyy, Txy, Tii, Txx_o, Tyy_o, Txy_o, Exx, Eyy, Exy, η_vep, P, phase, MatParam, dt
+)
+    Threads.@threads for j in axes(Exx, 2)
+        for i in axes(Exx, 1)
+            @inbounds Txx[i, j], Tyy[i, j], Txy[i, j], Tii[i, j], η_vep[i, j] = _compute_τij(
+                Txx_o[i,j],
+                Tyy_o[i,j],
+                Txy_o[i,j],
+                Exx[i,j],
+                Eyy[i,j],
+                Exy[i,j],
+                P[i,j],
+                phase[i,j],
+                MatParam,
+                dt,
+            )
+        end
+    end
+end
+
+function _compute_τij(Txx_o, Tyy_o, Txy_o, Exx, Eyy, Exy, P, phase, MatParam, dt)
+    args = (; dt=dt, P=P, τII_old=0.0)
+    εij = (Exx, Eyy, Exy)
+    τij_o = (Txx_o, Tyy_o, Txy_o)
+    Tij, Tii, η_vep = compute_τij(MatParam, εij, args, τij_o, phase)
+
+    return Tij[1], Tij[2], Tij[3], Tii, η_vep
+end
+
+# staggered grid
+function compute_τij!(
+    Txx, Tyy, Txy, Tii, Txx_o, Tyy_o, Txyv_o, Exx, Eyy, Exyv, η_vep, P, phase_center, phase_vertex, MatParam, dt
+)
+    Threads.@threads for j in axes(Exx, 2)
+        for i in axes(Exx, 1)
+            @inbounds Txx[i, j], Tyy[i, j], Txy[i, j], Tii[i, j], η_vep[i, j] = _compute_τij(
+                Txx_o[i,j],
+                Tyy_o[i,j],
+                Txyv_o,
+                Exx[i,j],
+                Eyy[i,j],
+                Exyv,
+                P[i,j],
+                phase_center,
+                phase_vertex,
+                MatParam,
+                dt,
+                i,
+                j
+            )
+        end
+    end
+end
+
+function _compute_τij(
+    Txx_o,
+    Tyy_o,
+    Txyv_o,
+    Exx,
+    Eyy,
+    Exyv,
+    Pt,
+    phase_center,
+    phase_vertex,
+    MatParam,
+    dt,
+    i,
+    j
+)
+
+    args = (; dt=dt, P=P, τII_old=0.0)
+    # gather strain rate
+    εij_v = (Exyv[i, j], Exyv[i + 1, j], Exyv[i, j + 1], Exyv[i + 1, j + 1]) # gather vertices around ij center
+    εij = (Exx, Eyy, εij_v)
+    # gather deviatoric stress
+    τij_v = (Txyv_o[i, j], Txyv_o[i + 1, j], Txyv_o[i, j + 1], Txyv_o[i + 1, j + 1]) # gather vertices around ij center
+    τij_o = (Txx_o, Tyy_o, τij_v)
+    # gather material phases
+    phases_v = (phase_vertex[i, j], phase_vertex[i + 1, j], phase_vertex[i, j + 1], phase_vertex[i + 1, j + 1]) # gather vertices around ij center
+    phases = (phase_center[i, j], phase_center[i, j], phases_v)
+    # update stress and effective viscosity
+    Tij, Tii, η_vep = compute_τij(MatParam, εij, args, τij_o, phase)
+
+    return Tij[1], Tij[2], Tij[3], Tii, η_vep
+
+end
+
+# ----------------------------------------------------------------------------------------
 
 ## Hellper functions
 @inline function staggered_tensor_average(x::NTuple{N,Union{T,NTuple{4,T}}}) where {N,T}
