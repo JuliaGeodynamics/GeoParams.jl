0ccdb1b9385f68781ca148e645b52f7a2a6b0047
albert-de-montserrat <albert.de.montserrat@gmail.com>
2022-11-29 10:10:36 +0100

    some AD tools

--- src/Utils.jl
+++ src/Utils.jl
@@ -85,3 +85,27 @@ end
 macro print(a1, a2)
     return :($(esc(a1)) === true ? println($(esc(a2))) : nothing)
 end
+
+
+# AutoDiff tools
+
+@inline function derivative(f::F, x::R) where {F,R<:Real}
+    T = typeof(ForwardDiff.Tag(f, R))
+    res = f(ForwardDiff.Dual{T}(x, one(x)))
+    f, ∂f∂x = if res != 0.0
+        res.value, res.partials.values[1]
+    else
+        0.0, 0.0
+    end
+    return f, ∂f∂x
+end
+
+@inline function jacobian(f, x::StaticArray)
+    T = typeof(ForwardDiff.Tag(f, eltype(x)))
+    result = ForwardDiff.static_dual_eval(T, f, x)
+    J = ForwardDiff.extract_jacobian(T, result, x)
+    f = extract_value(result)
+    return f, J
+end
+
+extract_value(result::SVector{N, ForwardDiff.Dual{Tag, T, N}}) where {N,T,Tag} = SVector{N,T}(result[i].value for i in 1:N)
