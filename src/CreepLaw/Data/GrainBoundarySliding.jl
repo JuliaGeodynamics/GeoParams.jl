d3855028c05f452c2a2c7ddd232e46c060c33cfa
Nils Medinger <NilsMedinger@gmx.de>
2023-02-15 16:09:02 +0100

    added second GBS law

--- src/CreepLaw/Data/GrainBoundarySliding.jl
+++ src/CreepLaw/Data/GrainBoundarySliding.jl
@@ -9,6 +9,7 @@ SetGrainBoundarySliding(name::String; kwargs...) = Transform_GrainBoundarySlidin
 
 # predefined grain boundary sliding laws are to be added in the dictionary as it is done for dislocation creep laws (see 'DislocationCreep.jl')!
 const GrainBoundarySliding_info = Dict([
+
 # Dry Olivine rheology, T < 1523 K
 (
     "Dry Olivine < 1523K | Hirth and Kohlstedt (2003)",
@@ -19,6 +20,35 @@ const GrainBoundarySliding_info = Dict([
             p=-2.0NoUnits,                        # grain size exponent
             A=6500MPa^(-3.5) * μm^3.0 * s^(-1),    # material specific rheological parameter
             E=400.0kJ / mol,                        # activation energy
+            V=18e-6m^3 / mol,                       # activation Volume
+            Apparatus=AxialCompression,
+        ),
+        MaterialParamsInfo(;
+            Comment="Checked values; not yet plots (NM)",
+            BibTex_Reference="
+                @article{hirth2004rheology,
+                title={Rheology of the Upper Mantle and the Mantle Wedge: A View from the Experimentalists},
+                author={Hirth, Greg and Kohlstedt, David},
+                journal={Inside the Subduction Factory},
+                volume={138},
+                pages={83--105},
+                year={2004},
+                publisher={Wiley Online Library}
+                }
+        "),
+    ),
+)
+
+# Dry Olivine rheology, T >= 1523 K
+(
+    "Dry Olivine >= 1523K | Hirth and Kohlstedt (2003)",
+    (
+        DiffusionCreep(;
+            Name="Dry Olivine >= 1523K | Hirth and Kohlstedt (2003)",
+            n=3.5NoUnits,                         # power-law exponent
+            p=-2.0NoUnits,                        # grain size exponent
+            A=4.7e10MPa^(-3.5) * μm^3.0 * s^(-1),    # material specific rheological parameter
+            E=600.0kJ / mol,                        # activation energy
             V=18e-6m^3 / mol,                       # activation Volume
             Apparatus=AxialCompression,
         ),
