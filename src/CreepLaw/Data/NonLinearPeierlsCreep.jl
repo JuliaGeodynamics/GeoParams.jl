00cd3cf170bd0ed547b81f160034eab6e7154627
Nils Medinger <NilsMedinger@gmx.de>
2023-05-15 12:03:39 +0200

    corrected A value for NonLinearPeierlsCreepLaw

--- src/CreepLaw/Data/NonLinearPeierlsCreep.jl
+++ src/CreepLaw/Data/NonLinearPeierlsCreep.jl
@@ -20,7 +20,7 @@ const NonLinearPeierlsCreep_info = Dict([
             q=1.0NoUnits,                         # exponent of water-fugacity
             o=0.5NoUnits,                        # grain size exponent
             TauP=5.9e9Pa,                         # Peierls stress
-            A=(5.7e11)MPa^(-2.0) * s^(-1.0),    # material specific rheological parameter
+            A=(1.4e-7)MPa^(-2.0) * s^(-1.0),    # material specific rheological parameter
             E=320.0kJ / mol,                        # activation energy
             Apparatus=AxialCompression,
         ),
