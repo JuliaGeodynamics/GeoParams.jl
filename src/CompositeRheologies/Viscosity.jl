266bb41ee47d950738bf34b49f49ecb493ea78e8
albert-de-montserrat <albert.de.montserrat@gmail.com>
2023-06-23 17:16:22 +0200

    fix some issues with type signatures

--- src/CompositeRheologies/Viscosity.jl
+++ src/CompositeRheologies/Viscosity.jl
@@ -1,3 +1,9 @@
+export compute_viscosity_εII,
+    compute_viscosity_εij,
+    compute_viscosity_τII,
+    compute_viscosity_τij,
+    compute_elastoviscosity
+
 # extract elements from composite rheology
 @inline elements(v::Union{CompositeRheology, Parallel}) = v.elements
 
@@ -5,28 +11,28 @@
 @inline _viscosity(τII, εII) = τII / (2  * εII)
 
 # compute effective "creep" viscosity from strain rate tensor
-function compute_viscosity_εII(v::AbstractCreepLaw, εII, args::Vararg{T, N}) where {T, N}
-    τII = compute_τII(v, εII, args...)
+function compute_viscosity_εII(v::AbstractCreepLaw, εII, args)
+    τII = compute_τII(v, εII, args)
     η = _viscosity(τII, εII)
     return η
 end
 
-function compute_viscosity_εij(v::AbstractCreepLaw, xx, yy, xy, args::Vararg{T, N}) where {T, N}
+function compute_viscosity_εII(v::AbstractCreepLaw, xx, yy, xy, args)
     εII = second_invariant(xx, yy, xy)
-    η = compute_viscosity_εII(v, εII, args...)
+    η = compute_viscosity_εII(v, εII, args)
     return η
 end
    
 # compute effective "creep" viscosity from deviatoric stress tensor
-function compute_viscosity_τII(v::AbstractCreepLaw, τII, args::Vararg{T, N}) where {T, N}
-    εII = compute_εII(v, τII, args...)
+function compute_viscosity_τII(v::AbstractCreepLaw, τII, args)
+    εII = compute_εII(v, τII, args)
     η = _viscosity(τII, εII)
     return η
 end
 
-function compute_viscosity_τij(v::AbstractCreepLaw, xx, yy, xy, args::Vararg{T, N}) where {T, N}
+function compute_viscosity_τII(v::AbstractCreepLaw, xx, yy, xy, args)
     τII = second_invariant(xx, yy, xy)
-    η = compute_viscosity_τII(v, τII, args...)
+    η = compute_viscosity_τII(v, τII, args)
     return η
 end
 
@@ -35,45 +41,55 @@ end
 @inline compute_elastoviscosity(v::ConstantElasticity, η, args::NamedTuple) = compute_elastoviscosity(v, η, args.dt)
 
 # compute effective "creep" viscosity from strain rate tensor given a composite rheology
-@inline function compute_viscosity_εII(v::CompositeRheology, εII, args::Vararg{T, N}) where {T, N}
+@inline function compute_viscosity_εII(v::CompositeRheology, εII, args)
     e = elements(v)
-    compute_viscosity_II(e, compute_viscosity_εII, εII, args...)
+    compute_viscosity_II(e, compute_viscosity_εII, εII, args)
+end
+
+@inline function compute_viscosity_εII(v::CompositeRheology, εII, args)
+    e = elements(v)
+    compute_viscosity_II(e, compute_viscosity_εII, εII, args)
 end
 
 # compute effective "creep" viscosity from deviatoric stress tensor given a composite rheology
-@inline function compute_viscosity_τII(v::CompositeRheology, τII, args::Vararg{T, N}) where {T, N}
+@inline function compute_viscosity_τII(v::CompositeRheology, τII, args)
     e = elements(v)
-    compute_viscosity_II(e, compute_viscosity_τII, τII, args...)
+    compute_viscosity_II(e, compute_viscosity_τII, τII, args)
 end
 
 # compute effective "creep" for a composite rheology where elements are in series
-@generated function compute_viscosity_II(v::NTuple{N1, Union{AbstractCreepLaw, Parallel}}, fn::F, II, args::Vararg{T, N2}) where {T, N1, N2, F}
+@generated function compute_viscosity_II(v::NTuple{N, Union{AbstractCreepLaw, Parallel}}, fn::F, II, args) where {F, N}
     quote
         Base.@_inline_meta
         η = 0.0
-        Base.@nexprs $N1 i -> η += inv(fn(v[i], II, args...)) * !iselastic(v[i]) * !isplastic(v[i])
+        Base.@nexprs $N i -> η += inv(fn(v[i], II, args)) * !iselastic(v[i]) * !isplastic(v[i])
         return inv(η)
     end
 end
 
 # compute effective "creep" viscosity from strain rate tensor given a composite rheology
-@inline function compute_viscosity_εII(v::Parallel, εII, args::Vararg{T, N}) where {T, N}
+@inline function compute_viscosity_εII(v::Parallel, εII, args)
     e = elements(v)
-    compute_viscosity_II_parallel(e, compute_viscosity_εII, εII, args...)
+    compute_viscosity_II_parallel(e, compute_viscosity_εII, εII, args)
 end
 
 # compute effective "creep" viscosity from deviatoric stress tensor given a composite rheology
-@inline function compute_viscosity_τII(v::Parallel, τII, args::Vararg{T, N}) where {T, N}
+@inline function compute_viscosity_τII(v::Parallel, τII, args)
     e = elements(v)
-    compute_viscosity_II_parallel(e, compute_viscosity_τII, τII, args...)
+    compute_viscosity_II_parallel(e, compute_viscosity_τII, τII, args)
 end
 
 # compute effective "creep" for a composite rheology where elements are in parallel
-@generated function compute_viscosity_II_parallel(v::NTuple{N1, AbstractCreepLaw}, fn::F, II, args::Vararg{T, N2}) where {T, N1, N2, F}
+@generated function compute_viscosity_II_parallel(v::NTuple{N, AbstractCreepLaw}, fn::F, II, args) where {F, N}
     quote
         Base.@_inline_meta
         η = 0.0
-        Base.@nexprs $N1 i -> η += fn(v[i], II, args...)
+        Base.@nexprs $N i -> η += fn(v[i], II, args)
         return η
     end
 end
+
+# compute_viscosity_εII(v::MaterialParams, εII, args) = compute_viscosity_εII(v.CompositeRheology, εII, args)
+# compute_viscosity_εij(v::MaterialParams, εII, args) = compute_viscosity_εij(v.CompositeRheology, εII, args)
+# compute_viscosity_τII(v::MaterialParams, τII, args) = compute_viscosity_τII(v.CompositeRheology, τII, args)
+# compute_viscosity_τij(v::MaterialParams, τII, args) = compute_viscosity_τij(v.CompositeRheology, τII, args)
