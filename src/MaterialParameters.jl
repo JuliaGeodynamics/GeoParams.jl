b977e5904f3b35de06579a9a50bd67309e567d48
albert-de-montserrat <albert.de.montserrat@gmail.com>
2023-06-26 11:10:41 +0200

    loop was outside the module...

--- src/MaterialParameters.jl
+++ src/MaterialParameters.jl
@@ -371,18 +371,21 @@ function print_composite(a, spaces=10)
     return str
 end
 
-end
 
 for fn in (:compute_viscosity_εII, :compute_viscosity_εij, :compute_viscosity_τII, :compute_viscosity_τij)
-    # single phase version
-    $fn(v::MaterialParams, args::Vararg{Any, N}) where {N} = $fn(v.CompositeRheology[1], args...)
+    @eval begin
+        # single phase version
+        $fn(v::MaterialParams, args::Vararg{Any, N}) where {N} = $fn(v.CompositeRheology[1], args...)
 
-    # multi-phase version
-    @generated function $fn(v::NTuple{N1, AbstractMaterialParamsStruct}, phase, args::Vararg{Any, N2}) where {N1, N2}
-        quote
-            Base.@nexprs $N1 i -> i == phase && (return $fn(v.CompositeRheology[i], args...))
-            return 0.0
+        # multi-phase version
+        @generated function $fn(v::NTuple{N1, AbstractMaterialParamsStruct}, phase, args::Vararg{Any, N2}) where {N1, N2}
+            quote
+                Base.@nexprs $N1 i -> i == phase && (return $fn(v.CompositeRheology[i], args...))
+                return 0.0
+            end
         end
     end
+end
+
 end
 
