4bd14a322f862d8a89e7c06b19b90cc69ee2cd58
Nils Medinger <NilsMedinger@gmx.de>
2023-09-12 19:13:37 +0200

    stuff that was implemented by merging by albert, need to be recommited because of git reset i guess

---
+++ src/Plotting/test_script.jl
@@ -0,0 +1,77 @@
+
+
+
+
+
+
+
+### Als nächstes versuchen die Farben in Thorstens plot nach zu bilden
+### ODER die A values zurück umrechnen und gucken ob sie zu Thorstens values passen oder nicht
+
+
+
+
+
+
+
+using Pkg
+Pkg.activate(".")
+using GeoParams
+using GLMakie
+using CairoMakie
+
+#x1 = SetGrainBoundarySliding("Dry Olivine < 1523K | Hirth and Kohlstedt (2003)")
+x1 = SetGrainBoundarySliding("TEST GBS")
+#x1 = SetGrainBoundarySliding("THORSTEN GBS")
+x1 = remove_tensor_correction(x1)
+#x2 = SetNonLinearPeierlsCreep("Dry Olivine | Mei et al. (2010)")
+x2 = SetNonLinearPeierlsCreep("TEST PEIERLS")
+#x2 = SetNonLinearPeierlsCreep("THORSTEN PEIERLS")
+x2 = remove_tensor_correction(x2)
+#x3 = SetDislocationCreep("Dry Olivine | Hirth & Kohlstedt (2003)")
+x3 = SetDislocationCreep("TEST DISL")
+#x3 = SetDislocationCreep("THORSTEN DISL")
+x3 = remove_tensor_correction(x3)
+#x4 = SetDiffusionCreep("Dry Olivine | Hirth & Kohlstedt (2003)")
+x4 = SetDiffusionCreep("TEST DIFF")
+#x4 = SetDiffusionCreep("THORSTEN DIFF")
+x4 = remove_tensor_correction(x4)
+c2 = CompositeRheology(x1, x2, x3, x4)
+
+# angepasste values damit der plot UNGEFÄHR passt. rausbekommen wie man von ursprünglichen values auf angepasste values kommt und warum.
+
+args = (P=1.0e9,)
+#args = (P=1.0e9, d=3.0)
+T = (0.0,1523.0)
+#T = (0.0,1623.0)
+σ = (1.0e-1, 1.0e4)
+d = (1.0e-6, 1.0e-1)
+
+p = PlotDeformationMap(c2, args=args, d=d, σ=σ, T=T, grainsize=true, colormap=:sienna, boundaries=false,
+                       categorical_cbar=true, categorical_cbar_labels=["GBS", "Peierls", "Dislocation", "Diffusion"])
+#p = PlotDeformationMap(c2, args=args, σ=σ, T=T, colormap=:sienna, boundaries=false,
+#                       categorical_cbar=true, categorical_cbar_labels=["Peierls", "Dislocation", "Diffusion"])
+
+#=
+function compute_εtestnoinvar(
+    a::GrainBoundarySliding, TauII::_T; T=one(precision(a)), P=zero(precision(a)), d=one(precision(a)), args...
+) where {_T}
+    @unpack_val n, p, A, E, V, R = a
+    FT, FE = a.FT, a.FE
+    @show n
+    @show p
+    @show A
+    @show E
+    @show V
+    @show R
+    @show FT
+    @show FE
+
+    ε = fastpow(TauII / fastpow(A, -1.0 / n), n) *
+        fastpow(d, p) *
+        exp(-(E + P * V) / (R * T))
+
+    return ε 
+end=#
+
+
