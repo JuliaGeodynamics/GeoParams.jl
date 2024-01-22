de3025c37a7f84518c56a43d3737614dda39342d
albert-de-montserrat <albert.de.montserrat@gmail.com>
2024-01-22 16:11:22 +0100

    new version of softening

--- src/Plasticity/Softening.jl
+++ src/Plasticity/Softening.jl
@@ -1,3 +1,4 @@
+
 abstract type AbstractSoftening end
 
 struct NoSoftening{T} <: AbstractSoftening end
@@ -9,21 +10,19 @@ struct LinearSoftening{T} <: AbstractSoftening
     min_value::T
     damage::T
 
-    function LinearSoftening(hi::T, lo::T, max_value::T, min_value::T) where T
+    function LinearSoftening(min_value::T, max_value::T, lo::T, hi::T) where T
         damage = max_value + (max_value - min_value) / (hi - lo)
         new{T}(hi, lo, max_value, min_value, damage)
     end
 end
 
-function (softening::LinearSoftening{T})(x, softening_var) where T
-    (; hi, lo, min_value, damage) = softening
+LinearSoftening(min_max_values::NTuple{2, T}, lo_hi::NTuple{2, T}) where T = @inline LinearSoftening(min_max_values..., lo_hi...)
+
+function (softening::LinearSoftening)(softening_var)
+    (; hi, lo, max_value, min_value, damage) = softening
 
     softening_var ≥ hi && return min_value
-    softening_var ≤ lo && return T(x)
+    softening_var ≤ lo && return max_value
     
     return softening_var * damage
 end
-
-# soft = LinearSoftening(1.0, 0.0, 30e0, 15e0)
-
-# soft(30e0, 0.5)
