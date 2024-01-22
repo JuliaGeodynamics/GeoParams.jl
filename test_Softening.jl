a00ee4b98fc5cff0c04e6b7083467435919d0862
albert-de-montserrat <albert.de.montserrat@gmail.com>
2024-01-22 17:49:21 +0100

    update tests

--- test_Softening.jl
+++ test_Softening.jl
@@ -1,6 +1,14 @@
 using GeoParams, Test
 
 @testset "LinearSoftening" begin
+
+    # Test NoSoftening
+    soft = NoSoftening()
+    x = rand()
+    @test x === soft(x, rand())
+
+
+    # Test LinearSoftening
     min_v, max_v = 15e0, 30e0
     lo, hi = 0.0, 1.0
     
@@ -8,19 +16,11 @@ using GeoParams, Test
     
     soft = LinearSoftening(min_v, max_v, lo, hi)
     
-    @test soft(1) == min_v
-    @test soft(0) == max_v
-    @test soft(0.5) == 0.5 * (min_v + max_v)
+    @test soft(30, 1) == min_v
+    @test soft(30, 0) == max_v
+    @test soft(30, 0.5) == 0.5 * (min_v + max_v)
+
+    # test plasticity structs
+    d = DruckerPrager(; softening_C = soft)
     
 end
-
-min_v, max_v = 15e0, 30e0
-lo, hi = 0.0, 1.0
-
-@test LinearSoftening(min_v, max_v, lo, hi) === LinearSoftening((min_v, max_v), (lo, hi))
-
-soft = LinearSoftening(min_v, max_v, lo, hi)
-
-@test soft(1) == min_v
-@test soft(0) == max_v
-@test soft(0.5) == 0.5 * (min_v + max_v)
