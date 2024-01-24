7bca8f7dad9d41d2c00de7ebcfb5b00afb00c331
Albert de Montserrat <58044444+albert-de-montserrat@users.noreply.github.com>
2024-01-24 10:22:12 +0100

    Fix softening bug (#154)
    * improve softening testing
    
    * fix bug and improve args handling

--- src/Softening/Softening.jl
+++ src/Softening/Softening.jl
@@ -10,20 +10,22 @@ struct LinearSoftening{T} <: AbstractSoftening
     lo::T
     max_value::T
     min_value::T
-    damage::T
+    slope::T
 
     function LinearSoftening(min_value::T, max_value::T, lo::T, hi::T) where T
-        damage = max_value + (max_value - min_value) / (hi - lo)
-        new{T}(hi, lo, max_value, min_value, damage)
+        slope = (max_value - min_value) / (hi - lo)
+        new{T}(hi, lo, max_value, min_value, slope)
     end
 end
 
 LinearSoftening(min_max_values::NTuple{2, T}, lo_hi::NTuple{2, T}) where T = LinearSoftening(min_max_values..., lo_hi...)
 
-function (softening::LinearSoftening)(max_value, softening_var::T) where T
+@inline (softening::LinearSoftening)(args::Vararg{Any, N}) where N = softening(promote(args...)...)
+
+@inline function (softening::LinearSoftening)(max_value::T, softening_var::T) where T
 
     softening_var ≥ softening.hi && return softening.min_value
-    softening_var ≤ softening.lo && return T(max_value)
+    softening_var ≤ softening.lo && return max_value
     
-    return softening_var * softening.damage
+    return softening.min_value + (one(T) - softening_var) * softening.slope
 end
