06646a2693c212f49ce69612178c1b3d958cddaf
Albert de Montserrat <58044444+albert-de-montserrat@users.noreply.github.com>
2024-01-27 20:37:04 +0100

    Renames `NoLinearSoftening` (#159)
    * typo
    
    * new tests
    
    * reworks internals & fixes segfault

--- test/test_Softening.jl
+++ test/test_Softening.jl
@@ -5,7 +5,7 @@ using GeoParams, Test
     # Test NoSoftening
     soft = NoSoftening()
     x = rand()
-    @test x === soft(x, rand())
+    @test x === soft(rand(), x)
 
     # Test LinearSoftening
     min_v, max_v = rand()*15, (rand()+1)*15
@@ -15,16 +15,16 @@ using GeoParams, Test
     
     soft_ϕ = LinearSoftening(min_v, max_v, lo, hi)
     
-    @test soft_ϕ(max_v, 1) == min_v
-    @test soft_ϕ(max_v, 0) == max_v
-    @test soft_ϕ(max_v, 0.5) ≈ 0.5 * (min_v + max_v)
+    @test soft_ϕ(1  , max_v) == min_v
+    @test soft_ϕ(0  , max_v) == max_v
+    @test soft_ϕ(0.5, max_v) ≈ 0.5 * (min_v + max_v)
 
     min_v, max_v = 20e0, 20e0
     soft_ϕ = LinearSoftening(min_v, max_v, lo, hi)
     
-    @test soft_ϕ(max_v, 1) == 20e0
-    @test soft_ϕ(max_v, 0) == 20e0
-    @test soft_ϕ(max_v, 0.5) == 20e0
+    @test soft_ϕ(1  , max_v) == 20e0
+    @test soft_ϕ(0  , max_v) == 20e0
+    @test soft_ϕ(0.5, max_v) == 20e0
 
     # test Drucker-Prager with softening
     min_v, max_v = 15e0, 30e0
@@ -37,7 +37,6 @@ using GeoParams, Test
 
     p = DruckerPrager()
     @test compute_yieldfunction(p, args) ≈ 1.0839745962155614e7
-
     args = (P=P, τII=τII, EII=1e0)
 
     p1 = DruckerPrager(; softening_ϕ = soft_ϕ)
@@ -62,4 +61,11 @@ using GeoParams, Test
     @test compute_yieldfunction(p2, args) ≈ 1.95e7
     @test compute_yieldfunction(p3, args) ≈ 1.974118095489748e7
     
+    # non linear softening 
+    p4 = DruckerPrager(; softening_C = NonLinearSoftening())
+    p5 = DruckerPrager(; softening_C = NonLinearSoftening(ξ₀ = 30, Δ = 10))
+
+    @test compute_yieldfunction(p4, args) ≈ 1.95e7
+    @test compute_yieldfunction(p5, (P=P, τII=τII, EII=0e0)) ≈ 1.9499974039493073e7
+    @test compute_yieldfunction(p5, (P=P, τII=τII, EII=10e0)) ≈ 1.9499982679491926e7
 end
