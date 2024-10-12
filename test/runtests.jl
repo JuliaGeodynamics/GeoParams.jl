280416204cfd41d660df97551e81b43a785881a6
Boris Kaus <kaus@uni-mainz.de>
2024-10-12 16:31:49 +0200

    remove debugging comment

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ function runtests()
     files = readdir(@__DIR__)
     test_files = filter(startswith("test_"), files)
 
-    for f in test_files[1]
+    for f in test_files
         if !isdir(f)
             include(f)
         end
