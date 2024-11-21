29702f7e6a6586c142dd5aba19fa734205b5a313
albert-de-montserrat <albert.de.montserrat@gmail.com>
2024-11-21 11:47:54 +0100

    typos

--- test/test_Gravity.jl
+++ test/test_Gravity.jl
@@ -11,14 +11,14 @@ using Test, GeoParams
     @test gconst.g == convert(GeoUnit,1)
 
     d = DippingGravity(90, 0, g)
-    gᵢ = compute_gravity2(d)
+    gᵢ = compute_gravity(d)
     @test gᵢ == (0e0, 0e0, g)
 
     d = DippingGravity(45, 0, g)
-    gᵢ = compute_gravity2(d)
+    gᵢ = compute_gravity(d)
     @test gᵢ == sind(90 - α) .* (g, 0, g)
 
     d = DippingGravity(45, 45, g)
-    gᵢ = compute_gravity2(d)
+    gᵢ = compute_gravity(d)
     @test gᵢ ==   sind(90 - α) .* (g *  cosd(45), g *  cosd(45), g)
 end
